a8a4cd2af8b50ace2a03613bfabae006a2d466c0
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2016-06-26 16:16:51 +0300

    поправил htaccess и адрес

--- pages/contacts.php
+++ pages/contacts.php
@@ -8,9 +8,11 @@
 		<b><?= COMPANY_NAME ?></b>
 	</p>
 	<p>
-		141207, г.Пушкино, ул. Авиационная, д.1.<br>
+		141207, г.Пушкино, ул. Авиационная, д.15.<br>
 		Тел.: <?= implode(', ', $phone_numbers)?> <span style="float:right">Круглосуточная поддержка:</span><br>
 		Время работы офиса: 9:00 до 18:00. <span style="float:right">E-mail: <?= EMAIL ?></span>
 	</p>
-	<div class="contacts_map"><script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=WH5wepkWrdCB6c38-PnbpkyzxQVwDuZh&width=685&height=513"></script></div>
+	<div class="contacts_map">
+		<script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=kwjt6Ik07vIu2EGweoed4rg2ytfhyX9r&width=685&height=513"></script>
+	</div>
 </div>
